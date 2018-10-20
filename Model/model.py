480bb4b3cec27ddbea4e76e5abe9267c59a15d90
Felipe Montoya Alvarez <fmontoyalvarez@gmail.com>
2018-10-20 14:33:17 -0500

    NN classifies numbers and letters

--- Model/model.py
+++ Model/model.py
@@ -1,11 +1,123 @@
+import math
 import os
 from os import walk
 import tensorflow as tf
+import cv2
+import numpy as np
+from tensorflow import keras
+import matplotlib.pyplot as plt
+import string
+TRAINING_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + '/Dataset/Caracteres/'
+fileNames=[]
+class_names_numbers = ['0', '1', '2', '3', '4','5', '6', '7', '8', '9']
+class_names_letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-f=[]
-for (dirpath, dirnames, filenames) in walk(BASE_DIR + '/Dataset/Caracteres'):
-    f.extend(filenames)
-    break
-f=[i[0] for i in f]
-print(f)
+def datafetching():
+    y = []
+    x = []    
+    for (dirpath, dirnames, filenames) in walk(TRAINING_DIR):
+        fileNames.extend(filenames)
+        y.append([i[0] for i in filenames]) 
+        x = [(cv2.resize(cv2.imread(TRAINING_DIR + f), (25,50))[:,:,0]/255) for f in filenames]
+        break
+    y= np.transpose(y)
+    y_nums = []
+    y_char = []
+    x_nums = []
+    x_char = []
+    for i in range(len(y)):
+        if(y.item(i).isdigit()):
+            y_nums.append(y[i])
+            x_nums.append(x[i])
+        else:
+            y_char.append(y[i])
+            x_char.append(x[i])
+    x_nums = np.array(x_nums)
+    y_nums = np.array(y_nums)
+    x_char = np.array(x_char)
+    y_char = [[ord(i[0])-97] for i in y_char]
+    y_char = np.array(y_char)
+    x_nums, y_nums = shuffle_in_unison(x_nums,y_nums)
+    x_char,y_char =shuffle_in_unison(x_char,y_char)
+    x_nums_training, x_nums_test = x_nums[:188,:], x_nums[188:,:]
+    y_nums_training, y_nums_test = y_nums[:188,:], y_nums[188:,:]
+    x_char_training, x_char_test = x_char[:188,:], x_char[188:,:]
+    y_char_training, y_char_test = y_char[:188,:], y_char[188:,:]
+    return x_nums_training,x_nums_test,y_nums_training,y_nums_test,x_char_training,x_char_test,y_char_training,y_char_test
+
+    
+    
+def shuffle_in_unison(a, b):
+    assert len(a) == len(b)
+    shuffled_a = np.empty(a.shape, dtype=a.dtype)
+    shuffled_b = np.empty(b.shape, dtype=b.dtype)
+    permutation = np.random.permutation(len(a))
+    for old_index, new_index in enumerate(permutation):
+        shuffled_a[new_index] = a[old_index]
+        shuffled_b[new_index] = b[old_index]
+    return shuffled_a, shuffled_b
+
+def trainNums(x_train,y_train,x_test,y_test):
+    model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(50, 25)),
+    keras.layers.Dense(128, activation=tf.nn.relu),
+    keras.layers.Dense(10, activation=tf.nn.softmax)
+    ])
+    model.compile(optimizer=tf.train.AdamOptimizer(), 
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+    model.fit(x_train, y_train, epochs=5)
+    ypred = model.predict(x_test)
+    ypred=np.argmax(ypred, axis =1)
+    print(ypred, np.transpose(y_test))
+    test_loss, test_acc = model.evaluate(x_test,y_test)
+    print('Test accuracy:', test_acc, '. Test loss: ', test_loss)
+
+def trainChar(x_train,y_train,x_test,y_test):
+    model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(50,25)),
+    keras.layers.Dense(128, activation=tf.nn.tanh),
+    keras.layers.Dense(128, activation=tf.nn.tanh),
+    keras.layers.Dense(128, activation=tf.nn.tanh),
+    keras.layers.Dense(26, activation=tf.nn.softmax)
+    ])
+    model.compile(optimizer=tf.train.AdamOptimizer(), 
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+    model.fit(x_train, y_train, epochs=10)
+    ypred = model.predict(x_test)
+    ypred=np.argmax(ypred, axis =1)
+    ypred=[chr(i+97) for i in ypred]
+    y_testi = [chr(i[0]+97) for i in y_test]
+    y_print = []
+    for i in range(len(ypred)):
+        y_print.append([ypred[i], y_testi[i]])
+    for i in range(len(ypred)):
+        if(y_print[i][0]!=y_print[i][1]):
+            plt.figure()
+            plt.xticks([])
+            plt.yticks([])
+            plt.grid(False)
+            plt.imshow(x_test[i], cmap=plt.cm.binary)
+            plt.xlabel(ypred[i])
+            plt.show()
+    test_loss, test_acc = model.evaluate(x_test,y_test)
+    print('Test accuracy:', test_acc, '. Test loss: ', test_loss)
+
+
+"""plt.figure(figsize=(10,10))
+for i in range(25):
+    plt.subplot(5,5,i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(x_nums[i], cmap=plt.cm.binary)
+    plt.xlabel(y_nums[i])
+plt.show()"""
+def main():
+    x_nums_training,x_nums_test,y_nums_training,y_nums_test,x_char_training,x_char_test,y_char_training,y_char_test = datafetching()
+    trainNums(x_nums_training,y_nums_training,x_nums_test,y_nums_test)
+    trainChar(x_char_training,y_char_training,x_char_test,y_char_test)
+
+if __name__ == '__main__':
+    main()
